a114f49959d4da3d5d431061b5c09f474006796b
nihonsan20 <nicolast6704@gmail.com>
2025-03-24 15:27:41 -0500

    Se modificaron las rutas agregandole el login y el registro para los clientes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,33 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ClientsController;
+
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pedido');
 });
+
+Route::resource('clients', ClientsController::class);
+
+
+Route::get('/pedido', function(){
+    $nombre = session('nombre');
+    return view('pedido', compact('nombre'));
+})->name('pedido');
+
+Route::post('/registro', [ClientsController::class, 'register'])->name('registro.store');
+Route::post('/login', [ClientsController::class, 'login'])->name('login.process');
+
+
+Route::post('/agregar-producto', [ClientsController::class, 'agregarProducto'])->name('client.agregarProducto');
+
+Route::post('/eliminar-producto', [ClientsController::class, 'eliminarProducto'])->name('client.eliminarProducto');
+
+
+Route::post('/enviar-pedido', [ClientsController::class, 'enviarPedido'])->name('client.enviarPedido');
+Route::get('/orden', [ClientsController::class, 'verOrden'])->name('client.verOrden');
+
+Route::get('/pedido-finalizado', function () {
+    return view('pedidoFinalizado');
+})->name('client.pedidoFinalizado');
